f066f89c448faabf690fa33939cac89422718dc9
gata <hayato.yamagata@icloud.com>
2024-06-05 16:05:06 +0900

    骨格まで完成

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 from ultralytics import YOLO
 import cv2
 
+pose = [
+        [10, 8],
+        [8, 6],
+        [9, 7],
+        [7, 5],
+        [16, 14],
+        [14, 12],
+        [15, 13],
+        [13, 11],
+        [12, 11],
+        [6, 12],
+        [5, 6],
+        [11, 5]
+        ]
+
 def pose_estimation_movie(filename_in, filename_out, model):
     """ 動画ファイルからリアルタイム物体検出する関数 """
     cap = cv2.VideoCapture(filename_in)
@@ -22,8 +37,10 @@ def pose_estimation_movie(filename_in, filename_out, model):
                 x, y = int(keypoints.data[0,each][0]), int(keypoints.data[0,each][1])
                 cv2.circle(frame, (x, y), 5, (0, 255, 255), thickness=3, lineType=cv2.LINE_AA)
 
-            # 推定結果をオーバーレイ
-
+            for line in pose:
+                start_pt = (int(keypoints.data[0,line[0]][0]), int(keypoints.data[0,line[0]][1]))
+                end_pt = (int(keypoints.data[0,line[1]][0]), int(keypoints.data[0,line[1]][1]))
+                cv2.line(frame, start_pt, end_pt, (0, 0, 255), thickness=2)
             # 物体検出結果画像を表示
             cv2.imshow("Movie", frame)
 
